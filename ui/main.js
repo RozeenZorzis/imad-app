dbf28bac578f801d686aaeccaf9c2ece6dcbe35d
RozeenZorzis <rozeenzorzis@gmail.com>
2017-08-29 13:18:23 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,11 @@ element.innerHTML='NEW VALUE';
 
 //move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10
+    img.tyle.marginLeft=marginLeft + 'px'; 
+}
 img.onclick= function(){
-    img.style.marginLeft='100px';
-    
+    var interval=setInterval(moveRight,100);
 };
