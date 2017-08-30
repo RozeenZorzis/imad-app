6116922e38bd50be272bed6b64a1a6dde98e7299
RozeenZorzis <rozeenzorzis@gmail.com>
2017-08-30 03:41:04 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,27 @@
-console.log('Loaded!');
-//change the text of the main-text div
-var element=document.getElementById('main-text');
-element.innerHTML='NEW VALUE';
+//counter code
+var button = document.getElementById('counter');
 
-//move the image
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft + 'px'; 
-}
-img.onclick= function(){
-    var interval=setInterval(moveRight,50);
+button.onclick = function(){
+    
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            //Tacke some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+        //Not done yet
+    };
+    
+    //Make the request
+    request.open('GET', 'http://rozeenzorzis.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
 };
