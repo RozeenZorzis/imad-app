0bf2751fe1e1212785b2d13633f014714ed389e1
RozeenZorzis <rozeenzorzis@gmail.com>
2017-08-30 05:25:55 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,14 +27,19 @@ button.onclick = function(){
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick=function(){
-    //make a request to the server and send the name
+    var submit = document.getElementById('submit_btn');
+    submit.onclick=function(){
+        //Create a request object
+    var request = new XMLHttpRequest();
     
-    //capture a list of names and render it as a list 
-    var names=['name1','name2','name3','name4'];
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Tacke some action
+            if(request.status === 200){
+               //capture a list of names and render it as a list 
+    var names=request.responseText;
+    names=JSON.parse(names);
     var list='';
     for(var i=0;i<names.length;i++){
         list+='<li>'+ names[i] +'</li>';
@@ -42,4 +47,18 @@ submit.onclick=function(){
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
+                
+            }
+        }
+        //Not done yet
+    };
+    
+    //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET', 'http://rozeenzorzis.imad.hasura-app.io/submit-name?name=' +name ,true);
+    request.send(null);
+    //make a request to the server and send the name
+    
+    
 };
