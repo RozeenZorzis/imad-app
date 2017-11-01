7db02b88e03b860e8f746a72335fcb600d5a22ad
RozeenZorzis <rozeenzorzis@gmail.com>
2017-11-02 03:23:54 +0530

    Update REGISTRATION.php

--- ui/REGISTRATION.php
+++ ui/REGISTRATION.php
@@ -5,3 +5,9 @@ $query = "INSERT INTO registration VALUES ('$_POST[Name]','$_POST[Age]',
 '$_POST[Contact_No]','$_POST[DOB]','$_POST[Sex]','$_POST[City]')";
 $result = pg_query($query); 
 ?>
+<?php
+$db = pg_connect("host=localhost port=5432 dbname=rozeenzorzis user=rozeenzorzis password=db-rozeenzorzis-27743
+");
+$query = "INSERT INTO User VALUES ('$_POST[Username]','$_POST[Password]')";
+$result = pg_query($query); 
+?>
